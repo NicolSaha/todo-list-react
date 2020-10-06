c69278e12ff0a3b36387c926026653e1370c849b
“[Nicol <[nicol.saha@gmail.com]>
2020-10-06 15:59:52 +0200

    Add class Todos and TodoItem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './Components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Todo List Item 1',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Todo List Item 2',
+            completed: true
+        },
+        {
+            id: 3,
+            title: 'Todo List Item 3',
+            completed: false
+        }
+    ]
+  }
+
+  // TOGGLE COMPLETE
+  markComplete = (id) => {
+      this.setState({ todos: this.state.todos.map(todo => {
+          if(todo.id === id)  {
+              todo.completed = !todo.completed
+          }
+          return todo;
+          }) });
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+          <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
 }
 
 export default App;
