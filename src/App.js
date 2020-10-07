87aee40ebc55d3d6328299fe8516e6c81e29db3c
“[Nicol <[nicol.saha@gmail.com]>
2020-10-07 11:32:56 +0200

    Add UUID, add pages section in components with about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Components/Layout/Header';
 import Todos from './Components/Todos';
 import AddTodo from './Components/AddTodo';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
@@ -9,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
         {
-            id: 1,
+            id: uuidv4(),
             title: 'Todo List Item 1',
             completed: false
         },
         {
-            id: 2,
+            id: uuidv4(),
             title: 'Todo List Item 2',
             completed: true
         },
         {
-            id: 3,
+            id: uuidv4(),
             title: 'Todo List Item 3',
             completed: false
         }
@@ -41,14 +42,26 @@ class App extends Component {
       this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
     }
 
+// ADD TODO
+    addTodo = (title) => {
+      const newTodo = {
+          id: uuidv4(),
+          title,
+          completed: false
+      }
+      this.setState({todos: [...this.state.todos, newTodo]})
+    }
+
   render() {
 
     return (
       <div className="App">
-          <Header />
-          <AddTodo />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo = {this.deleteTodo} />
-      </div>
+          <div className="container">
+              <Header />
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo = {this.deleteTodo} />
+          </div>
+     </div>
     );
   }
 }
